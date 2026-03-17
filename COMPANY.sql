8ce957564651ee8bb0b72bf504292efb7ee35e2a
Kathoni <thrusherkeys@gmail.com>
2026-03-17 09:57:35 +0300

    use of join

--- COMPANY.sql
+++ COMPANY.sql
@@ -244,3 +244,24 @@ UNION
 SELECT salary
 FROM employee;
 
+-- use of join
+-- Add the extra branch
+INSERT INTO branch VALUES(4, "Buffalo", NULL, NULL);
+
+-- Find all branches and names of their managers
+SELECT employee.emp_id, employee.first_name, branch.branch_name
+FROM employee
+JOIN branch 
+ON employee.emp_id = branch.mgr_id;
+
+-- LEFT JOIN, RIGHT JOIN
+SELECT employee.emp_id, employee.first_name, branch.branch_name
+FROM employee
+LEFT JOIN branch 
+ON employee.emp_id = branch.mgr_id;
+
+SELECT employee.emp_id, employee.first_name, branch.branch_name
+FROM employee
+RIGHT JOIN branch 
+ON employee.emp_id = branch.mgr_id;
+
