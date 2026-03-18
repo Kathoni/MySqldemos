af634b7c3b96920857e029d0dfd07c09dedb82a2
Kathoni <thrusherkeys@gmail.com>
2026-03-18 10:34:45 +0300

    py dfs

--- arrays.py
+++ arrays.py
@@ -11,3 +11,18 @@ for word in words:
     freq[word] = freq.get(word, 0) + 1
 
 print(freq)
+
+import pandas as pd
+
+
+data = {
+    "student_id": [101, 53, 128, 3],
+    "name": ["Ulysses", "William", "Henry", "Diana"],
+    "age": [13, 10, 6, 11]
+}
+students = pd.DataFrame(data)
+
+print(students)
+
+filtered = students.loc[students["student_id"] == 101, ["name", "age"]]
+print(filtered)
